449a8fcdc4a32649b35f1413059568e9e253d159
marquasiahayes <hayes.marquasia@yahoo.com>
2017-07-20 15:33:38 -0400

    Add files via upload

---
+++ script.js
@@ -0,0 +1,14 @@
+function results1(){
+if(document.getElementById('suspense').checked==true) {
+  document.getElementById('result').innerHTML = "you chose suspense"
+}else if(document.getElementById('drama').checked==true) {
+  document.getElementById('result').innerHTML = "you chose drama"
+}else if(document.getElementById('horror').checked==true) {
+document.getElementById('result').innerHTML = "you chose Horror"
+}
+if(document.getElementById('karma').checked==true) {
+  document.getElementById('result').innerHTML = "you chose karma"
+}else if(document.getElementById('justice').checked==true) {
+  document.getElementById('result').innerHTML = "you chose justice"
+}else if(document.getElementById('revenge').checked==true) {
+document.getElementById('result').innerHTML = "you chose revenge"
